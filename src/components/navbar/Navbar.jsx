d5d826197e832e80b2f4c0c38164a344e00dfb2e
Brendan Merritt <brm2000@live.unc.edu>
2023-01-27 03:53:14 -0500

    created hamburger menu for navbar on mobile (<1280px)

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import "../../styles/navbar.css";
 import { useNavigate } from "react-router-dom";
 import { loadImage } from "../../utils/imageLoader";
+import { TfiClose } from "react-icons/tfi";
+import { SlMenu } from "react-icons/sl";
+import { useState, useEffect } from "react";
 
 export default function Navbar(props) {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1280);
+  const [isOpen, setIsOpen] = useState(false);
+
   let navigate = useNavigate();
-  const img = props.color === "black" ? "black_281" : "white_281"
+  const img = props.color === "black" ? "black_281" : "white_281";
+
+  const updateMedia = () => {
+    setIsDesktop(window.innerWidth >= 1280);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
 
   return (
     <div className="navbar">
@@ -15,7 +30,31 @@ export default function Navbar(props) {
           className="logo"
         />
       </div>
-      <ul className="navContainer">
+      {!isDesktop && (
+        <div style={{ display: "flex", alignItems: "center", gap: "1vw", color: props.color }}>
+          <span
+            style={{ fontSize: "1.2vw", fontWeight: "300", cursor: "pointer" }}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            MENU
+          </span>
+          <SlMenu
+            className="navMenuButton"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
+      )}
+      <ul className={isOpen ? "navContainer show" : "navContainer hide"}>
+        {!isDesktop ? (
+          <div className="mobileNavClose">
+            <TfiClose
+              onClick={() => setIsOpen(!isOpen)}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+        ) : (
+          ""
+        )}
         <li
           onClick={() => {
             navigate("/");
@@ -64,7 +103,9 @@ export default function Navbar(props) {
         <li
           onClick={() => {
             navigate("/contact");
-            window.location.reload(false); /* CHANGE LATER, THIS IS SO HACKY AND BAD */
+            window.location.reload(
+              false
+            ); /* CHANGE LATER, THIS IS SO HACKY AND BAD */
           }}
           className="navItem"
           id={props.color}
