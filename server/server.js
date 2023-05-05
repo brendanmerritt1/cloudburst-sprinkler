9b1f6a3326e8a9582dd60e7b7d617402e81a1380
Brendan Merritt <brm2000@live.unc.edu>
2023-05-05 13:37:27 -0400

    VPS Deployment

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const axios = require("axios");
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.REACT_APP_SG_API_KEY);
-const path = require("path");
+const nodemailer = require("nodemailer");
 const port = 4000;
 
 const app = express();
@@ -13,72 +11,60 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
-  });
-}
+app.post("/api/recaptcha", async (req, res) => {
+  const { token } = req.body;
+  await axios.post(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.REACT_APP_RECAPTCHA_SECRETKEY}&response=${token}`
+  );
 
-app.use(express.static("public"));
+  if (res.status(200)) {
+    res.send("reCAPTCHA verified successfully.");
+  } else {
+    res.send("reCAPTCHA verification failed - robot detected.");
+  }
+});
 
-app.post(
-  path.join(process.env.REACT_APP_BACKEND_URL, process.env.REACT_APP_API_URL),
-  async (req, res) => {
-    const { token } = req.body;
-    await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.REACT_APP_RECAPTCHA_SECRETKEY}&response=${token}`
-    );
+let transporter = nodemailer.createTransport({
+  host: "smtp.sendgrid.net",
+  port: 587,
+  auth: {
+    user: "apikey",
+    pass: process.env.REACT_APP_SG_API_KEY,
+  },
+});
 
-    if (res.status(200)) {
-      res.send("reCAPTCHA verified successfully.");
+transporter.verify((err, success) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`=== Server is ready to take messages: ${success} ===`);
+  }
+});
+
+app.post("/api/contact", (req, res) => {
+  let mailOptions = {
+    from: `${req.body.formState.firstName} ${req.body.formState.lastName} <${process.env.REACT_APP_EMAIL}>`,
+    to: process.env.REACT_APP_EMAIL,
+    replyTo: `${req.body.formState.email}`,
+    subject: `Message from: ${req.body.formState.firstName} ${req.body.formState.lastName}`,
+    text: `
+    ${req.body.formState.message}
+
+    Project type: ${req.body.formState.project}
+    Phone number: ${req.body.formState.phone}
+    Affiliated company: ${req.body.formState.company}
+    Current Cloudburst Sprinkler customer: ${req.body.formState.customer}
+  `,
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.error(err);
     } else {
-      res.send("reCAPTCHA verification failed - robot detected.");
+      console.log("Email sent successfully: " + info.response);
     }
-  }
-);
-
-app.post(
-  path.join(
-    process.env.REACT_APP_BACKEND_URL,
-    process.env.REACT_APP_SUBMIT_URL
-  ),
-  (req, res) => {
-    const msg = {
-      from: {
-        email: process.env.REACT_APP_EMAIL,
-        name: `${req.body.formState.firstName} ${req.body.formState.lastName}`,
-      },
-      to: {
-        email: process.env.REACT_APP_EMAIL,
-        name: "Cloudburst Lawn Sprinkler Systems",
-      },
-      replyTo: {
-        email: `${req.body.formState.email}`,
-      },
-      subject: `Message from: ${req.body.formState.firstName} ${req.body.formState.lastName}`,
-      text: `
-        ${req.body.formState.message}
-      
-        Project type: ${req.body.formState.project}
-        Phone number: ${req.body.formState.phone}
-        Affiliated company: ${req.body.formState.company}
-        Current Cloudburst Sprinkler customer: ${req.body.formState.customer}
-      `,
-    };
-
-    sgMail
-      .send(msg)
-      .then((res) => {
-        console.log(
-          "Email sent successfully. Status code: " + res[0].statusCode
-        );
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }
-);
+  });
+});
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on port ${process.env.PORT || port}`);
